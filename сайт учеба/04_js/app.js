df2851b3421e06f3667304dbd85cd6efd77464bf
Natalia <dreamstrueok@gmail.com>
2023-08-16 19:04:45 +0300

    ButHide

--- сайт учеба/04_js/app.js
+++ сайт учеба/04_js/app.js
@@ -1,6 +1,8 @@
 let cardNodeList = document.querySelectorAll('.card_box'); 
 let card_boxArr = [...cardNodeList];
 let ButShow = document.querySelector('#add');
+let ButHide = document.querySelector('#hide');
+    ButHide.hidden = true;
 
 for (let i = 0; i < card_boxArr.length; i++){
      if (i > 3){
@@ -8,16 +10,29 @@ for (let i = 0; i < card_boxArr.length; i++){
      } 
 };
 
-let cardNext = card_boxArr.slice (4, );
 ButShow.addEventListener("click", function (){
+    let cardNext = card_boxArr.slice (4, );
     for (let card of cardNext){
         if (cardNext.length === 0) {
-        ButShow.style.display = "none";
+        ButShow.hidden = true;
+        ButHide.hidden = false;
         }
             console.log (card);
             card.hidden = false;
             cardNext = cardNext.slice(4,);
             };
+});
+
+ButHide.addEventListener("click", function (){
+    card_boxArr = [...cardNodeList];
+    for (let i = 0; i < card_boxArr.length; i++){
+        if (i > 3){
+           card_boxArr[i].hidden = true;
+        } 
+   };
+    ButShow.hidden = false;
+    ButHide.hidden = true;
+
 });
 
 
