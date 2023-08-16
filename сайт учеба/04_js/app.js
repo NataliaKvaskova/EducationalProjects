28f390a9374cc1af4587668ded46be5ca5b81074
Natalia <dreamstrueok@gmail.com>
2023-08-16 16:49:31 +0300

    first_open_page

--- сайт учеба/04_js/app.js
+++ сайт учеба/04_js/app.js
@@ -1,23 +1,17 @@
-// Получаю данные из html в NodeList 
 let cardNodeList = document.querySelectorAll('.card_box'); 
-// Формирую массив из элементов NodeList 
-const cardArr = [];
-for (let i of cardNodeList){
-    cardArr.push(i);
-}
-// Объявляю переменные для контейнера и кнопки
-let cardCont = document.querySelector("#cards");
-let addCard = document.querySelector('#add');
 
-// вывожу часть массива по кнопке
-addCard.addEventListener("click", function (){
-    let cardVisible = cardArr.splice(0,3);
-    console.log(cardVisible);
-    cardCont.insertAdjacentHTML("beforeend", cardVisible);
-    if (cardVisible.length === 0) {
-        addCard.style.display = "none";
-      }
-});
+let card_boxArr = [...cardNodeList];
+console.log (card_boxArr);
+console.log ("___________");
 
 
+for (let i = 0; i < card_boxArr.length; i++){
+     if (i > 3){
+        console.log (card_boxArr[i])
+        card_boxArr[i].hidden = true;
+
+     }
+ 
+    
+};
 
