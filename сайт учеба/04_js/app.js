ddf559581b6081d71368cd36358d1070cf6bbcbe
Natalia <dreamstrueok@gmail.com>
2023-08-16 18:16:08 +0300

    ButClick

--- сайт учеба/04_js/app.js
+++ сайт учеба/04_js/app.js
@@ -1,17 +1,24 @@
 let cardNodeList = document.querySelectorAll('.card_box'); 
-
 let card_boxArr = [...cardNodeList];
-console.log (card_boxArr);
-console.log ("___________");
-
+let ButShow = document.querySelector('#add');
 
 for (let i = 0; i < card_boxArr.length; i++){
      if (i > 3){
-        console.log (card_boxArr[i])
         card_boxArr[i].hidden = true;
-
-     }
- 
-    
+     } 
 };
+
+let cardNext = card_boxArr.slice (4, );
+ButShow.addEventListener("click", function (){
+    for (let card of cardNext){
+        if (cardNext.length === 0) {
+        ButShow.style.display = "none";
+        }
+            console.log (card);
+            card.hidden = false;
+            cardNext = cardNext.slice(4,);
+            };
+});
+
+
 
